a215e89cd6ddad4c0ecbcbaab0684499df4fd944
Michael Hansen <49969626+mhansenite@users.noreply.github.com>
2025-02-26 19:43:05 -0700

    update

--- modules/subnets/outputs.tf
+++ modules/subnets/outputs.tf
@@ -1,8 +1,3 @@
-output "app_subnet_map" {
-    value = local.app_subnet_map
-}
-output "db_subnet_map" {
-    value = local.db_subnet_map
-}
+# Outputs will be added when subnet mappings are implemented
 
 
